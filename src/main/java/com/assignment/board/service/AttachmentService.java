ed00a64bc9fefa0c8d209efa6beba88f008ad588
guluming <guluming@naver.com>
2022-08-11 23:04:17 +0900

    Feat: "추가 게시글 파일 업로드 및 삭제 기능"
    - 게시글 작성시 3개의 목업 파일 생성 기능 추가
    - 게시글 삭제시 연관관계에 있는 목업 파일 3개를 삭제하기 위한 기능 추가

---
+++ src/main/java/com/assignment/board/service/AttachmentService.java
@@ -0,0 +1,41 @@
+package com.assignment.board.service;
+
+import com.assignment.board.entity.Article;
+import com.assignment.board.entity.Attachment;
+import com.assignment.board.repository.AttachmentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Service
+public class AttachmentService {
+    private final AttachmentRepository attachmentRepository;
+
+    @Transactional
+    public ResponseEntity<?> registerFile(Long articleId, MultipartFile file) {
+
+        return new ResponseEntity<>("파일이 업로드 되었습니다.", HttpStatus.CREATED);
+    }
+
+    @Transactional
+    public void sampleRegisterFile(Article article) {
+        for (int i = 0; i < 3; i++) {
+            String location = "test url " + i;
+            Attachment attachment = new Attachment(article, location);
+            attachmentRepository.save(attachment);
+        }
+    }
+
+    @Transactional
+    public void sampleDeleteFile(Long articleId) {
+        List<Attachment> attachmentList = attachmentRepository.findByArticleId(articleId);
+        attachmentRepository.deleteAll(attachmentList);
+    }
+}
